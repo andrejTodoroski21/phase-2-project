64be2f9f05060b43190b9b7cd48ebf9666b4d624
Andrej <andrejtodoroski@yahoo.com>
2024-10-16 19:37:38 -0400

    more css things

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,41 +1,40 @@
-// this route should be a home page and not display the workouts, that will be in another route
-
 // import React from "react";
-// import WorkoutContainer from "../WorkoutContainer"
-// import { useState, useEffect } from "react";
-
 
 // function Home(){
-//     const [workouts, setWorkouts] = useState([])
-
-//   useEffect(()=> {
-//     fetch('api/workouts')
-//     .then(response => response.json())
-//     .then(workout => setWorkouts(workout))
-//   },[])
-//   console.log(workouts)
-//     return (
-//         <div>
-//             <div className='flex-container'>
-//           <WorkoutContainer workouts={workouts} setWorkouts={setWorkouts}/>
-//         </div>
-  
-//         </div>
-//     )
+//   return (
+//     <div>
+//       <video autoPlay muted loop id="home-video">
+//         <source src="https://assets.newyorksportsclubs.com/wp-content/uploads/2023/10/04072014/Sizzle-Reel-60-second.mp4" type="video/mp4" />
+//       </video>
+//       <div id="text-overlay">
+//         <h1>Build your better self</h1>
+//       </div>
+//     </div>
+//   )
 // }
+
 // export default Home
-
 import React from "react";
-import Navbar from "./NavBar";
+import { useNavigate } from "react-router-dom";
 
-function Home(){
+function Home() {
+  const navigate = useNavigate();
+
+  function redirect() {
+    navigate("/login");
+  }
   return (
-    <div>
+    <div className="home-container">
       <video autoPlay muted loop id="home-video">
         <source src="https://assets.newyorksportsclubs.com/wp-content/uploads/2023/10/04072014/Sizzle-Reel-60-second.mp4" type="video/mp4" />
       </video>
+
+      <div className="overlay-text">
+        <h1 className="hero-text">Build a better you</h1>
+        <button className="program-btn" onClick={redirect}>Start Your Program</button>
+      </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
